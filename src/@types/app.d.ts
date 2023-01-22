82f16d02afa5652e316ac71b60915fb5a7120458
Icaro Santos <39846852+Icar0S@users.noreply.github.com>
2023-01-21 22:06:55 -0300

    Delete src directory

--- src/@types/app.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="nativewind/types" />
-
-//para funcionar o "className"
--- src/lib/axios.ts
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export const api = axios.create({
-  baseURL: 'http://192.168.1.67:3333'//192.168.1.67 ou ..0.116 
-})
